11a7647772d7c0e31cebcfce15f6839e8b931336
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-05-30 01:52:05 +0530

    update : user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,16 +18,43 @@ class UserSeeder extends Seeder
             'email' => 'shop@doondukan.com',
             'phone' => 9876543210,
             'password' => Hash::make('123456'),
-            'role' => 'user',
-            'created_at' => Carbon::create(2019,1,1)
+            'role' => 'SHOPKEEPER',
+            'created_at' => Carbon::create(2021,1,1),
+            'updated_at' => Carbon::create(2021,1,1)
+        ]);
+
+        DB::table('stores')->insert([
+            'user_id' => 1,
+            'name' => 'shop',
+            'slug' => "shop",
+            'email' => 'shop@doondukan.com',
+            'mobile' => 9876543210,
+            'address' => 'address',
+            'about' => 'about',
+            'logo' => '',
+            'registration_date' =>  Carbon::create(2021,1,1),
+            'valid_upto' => Carbon::create(2022,1,1),
+            'created_at' => Carbon::create(2021,1,1),
+            'updated_at' => Carbon::create(2021,1,1),
+            'open_at' => "10:00:00",
+            'close_at' => "20:00:00",
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'user ',
+            'email' => 'user@gmail.com',
+            'phone' => 8954836965,
+            'password' => Hash::make('123456'),
+            'role' => 'USER',
+            'created_at' => Carbon::create(2021,1,1)
         ]);
 
         DB::table('admins')->insert([
             'name' => 'Admin',
             'email' => 'admin@doondukan.com',
             'password' => Hash::make('admin'),
-            'created_at' => Carbon::create(2019,1,1),
-            'updated_at' => Carbon::create(2019,1,1)
+            'created_at' => Carbon::create(2021,1,1),
+            'updated_at' => Carbon::create(2021,1,1)
 
         ]);
     }
