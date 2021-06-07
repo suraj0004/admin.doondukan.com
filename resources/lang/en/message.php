acd77f8920b6eca0a048f3cfc996ab04e52ee50c
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-06-08 02:09:41 +0530

    shop link and qr code data in dashboard api

---
+++ resources/lang/en/message.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "share_shop" => [
+        "link" => "https://app.doondukan.com/:seller_id-:shop_slug",
+        "message" => "Hello, shoppers!\nYou can now order online from :shop using this link -> :link \nContact us at :mobile if you need any help with ordering online.\nThank you."
+    ]
+];
