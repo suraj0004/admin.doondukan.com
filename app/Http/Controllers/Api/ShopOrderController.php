d06e8dcfb34ce748c616226bcc2254f639df0743
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-06-09 00:30:31 +0530

    order complete api

--- app/Http/Controllers/Api/ShopOrderController.php
+++ app/Http/Controllers/Api/ShopOrderController.php
@@ -5,7 +5,13 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
-
+use App\Models\Orders;
+use App\Models\OrderItem;
+use App\Models\Bill;
+use App\Models\Stock;
+use App\Models\Purchase;
+use App\Models\Sale;
+use DB;
 
 class ShopOrderController extends Controller
 {
@@ -32,28 +38,79 @@ class ShopOrderController extends Controller
      */
     public function updateStatus(Request $request)
     {
-      
+        // validation missing
+        // Need to refactor
         $orderId  =  $request->id;
         $status   = $request->status;
-        // aprint($request->all());
-        $orderData   =  \App\Models\Orders::whereId($orderId)->update(['status'=>$status]);
 
-        $orderProducts = \App\Models\Orders::with(['orderitem'])->whereId($orderId)->first();
-
-        
-        // aprint($orderProducts->orderitem);
-        // exit;
-
-
-        foreach ($orderProducts->orderitem as $key => $value) {
-            # code...
-
-            $updateQuant = \App\Models\Stock::whereProductId($value->product_id)->whereUserId($orderProducts->seller_id)
-                           ->decrement('quantity', $value->quantity);
+        if($status != 2){
+            Orders::whereId($orderId)->update(['status'=>$status]); // where user id missing
+            return response()->json(['statusCode'=>200,'success'=>true,'message'=>'Bill Generated Successfully.'], 200);
         }
 
 
-        return ($request->id);
+       $orderProducts = OrderItem::select('stocks.id as stock_id','order_items.quantity')
+        ->where('order_items.order_id',$orderId)
+        ->join('stocks',function ($join){
+            $join->on("order_items.product_id" , "=", "stocks.product_id")
+            ->on("stocks.user_id","=",DB::raw(Auth::id()));
+        })
+        ->get();
+
+
+        $user = Auth::User();
+    	$stock_details = [];
+    	foreach ($orderProducts as $key => $value)
+    	{
+    		$checkStock = Stock::where('id',$value['stock_id'])->where('quantity','>=',$value['quantity'])->where('user_id',$user->id)->first();
+	    	if (!$checkStock)
+			{
+				$message = "Quantity or Stock is unavailable.";
+			    return response()->json(['statusCode'=>200,'success'=>false,'message'=>$message], 200);
+			}
+			$stock_details[] = $checkStock;
+    	}
+
+    	$setCustomerbill = new Bill();
+    	$setCustomerbill->user_id = $user->id;
+    	$bill_ID = $setCustomerbill->save();
+    	if( $bill_ID )
+    	{
+    		foreach ($stock_details as $key => $stockvalue)
+    		{
+    			$stockvalue->quantity = $stockvalue->quantity - $orderProducts[$key]['quantity'];
+    			if( $stockvalue->save() )
+    			{
+                    //Get the last purchase price.
+                    $purchase_price = Purchase::select('id','price')->where('product_id',$stockvalue->product_id)->orderBy('id','desc')->limit(1)->first();
+
+    				$setSale = new Sale();
+	    			$setSale->user_id = $user->id;
+	    			$setSale->product_id = $stockvalue->product_id;
+	    			$setSale->bill_id = $setCustomerbill->id;
+	    			$setSale->quantity = $orderProducts[$key]['quantity'];
+	    			$setSale->price = $stockvalue->price;
+	    			$setSale->product_source = $stockvalue->product_source;
+                    $setSale->purchase_price = $purchase_price->price;
+	    			if( !$setSale->save() )
+	    			{
+	    				return response()->json(['statusCode'=>200,'success'=>false,'message'=>'Oops! Something Went Wrong!'], 200);
+	    			}
+    			}
+    			else
+    			{
+    				return response()->json(['statusCode'=>200,'success'=>false,'message'=>'Oops! Something Went Wrong!'], 200);
+    			}
+    		}
+
+            Orders::whereId($orderId)->update(['status'=>$status]); // where user id missing
+
+    		return response()->json(['statusCode'=>200,'success'=>true,'message'=>'Bill Generated Successfully.','data'=>$setCustomerbill->id], 200);
+    	}
+    	else
+    	{
+    		return response()->json(['statusCode'=>200,'success'=>false,'message'=>'Oops! Something Went Wrong!'], 200);
+    	}
 
     }
 
