fb7106e8835661ecfd486ddffb092f8afe0f36c4
VipinFarswan <vipinfarswan421@gmail.com>
2021-06-09 23:45:16 +0530

    add user/shopkeeper check in login,add email notification checkk for shoopkeeper

---
+++ app/Listeners/SendOrderPlacedNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPlaced;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Mail\OrderPlacedMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendOrderPlacedNotification implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPlaced  $event
+     * @return void
+     */
+    public function handle(OrderPlaced $event)
+    {
+        Mail::to($event->orderData['sellerEmail'])->send(new OrderPlacedMail($event->orderData));
+    }
+}
