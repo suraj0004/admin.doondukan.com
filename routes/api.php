d6c4076801b5ff3d80f17e007ffabe68252a13da
suraj0004 <38535596+suraj0004@users.noreply.github.com>
2021-06-19 04:26:36 +0530

    api route for ecommerce sign up otp

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Api\UserController@login');
 Route::post('register', 'Api\UserController@register');
 Route::post('send-otp', 'Api\OtpController@sendOTP');
+Route::post('ecommerce/send-otp', 'Api\OtpController@sendOTP');
 Route::post('forgot-password', 'Api\ForgotPasswordController@resetPassword');
 Route::group(['middleware' => ['auth:api','shoopkeeper'],'prefix'=>'retail'], function()
 {
